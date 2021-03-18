d146a17e26951ee4534cdbfadde6e9809a0dacc5
Issabek <180107287@stu.sdu.edu.kz>
2021-03-18 19:09:47 +0600

    FirstTry

---
+++ MIG.ExchangeRate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MIG.ExchangeRate
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
